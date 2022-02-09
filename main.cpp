660085cdf339b157ce6b39531c2249e7e2a00389
wilmerbanquez <wilmer.banquez@udea.edu.co>
2022-02-09 13:48:36 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    char palabra[25];
+    cout << "Ingrese una palabra: "; cin >> palabra;
+    int res;
+    char bin[9];
+    bin[8] = '\0';
+    int aux = 7;
+    for(int i = 0; palabra[i]!='\0';i++){
+        while(palabra[i]>0){
+            res = palabra[i]%2;
+            palabra[i] = palabra[i]/2;
+            bin[aux] = res + 48;
+            aux--;
+        }
+        for(int i = 0;i<7;i++){
+            if(bin[0] != 48 or bin[0]!= 49)
+                bin[0] = 48;
+        }
+        cout << bin;
+        aux = 7;
+    }
+    return 0;
+}
